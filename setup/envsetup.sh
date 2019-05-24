27bd8521f7551a2716e9e24e1798e82d6ccd84a0
AndyDinh <39890974+AndyDinh@users.noreply.github.com>
2019-05-24 00:50:14 -0400

    Update envsetup.sh

--- setup/envsetup.sh
+++ setup/envsetup.sh
@@ -5,6 +5,7 @@
 echo "Setting up environment folder for aardvark and repokid..."
 wget https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz
 
+sudo apt install git -y || sudo yum install git -y
 sudo apt install awscli -y || sudo yum install awscli -y
 sudo apt install python2.7 -y || sudo yum install python2.7 -y
 sudo apt install python2-pip -y || sudo yum install python2-pip -y
