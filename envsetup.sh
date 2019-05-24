eec9ed740aec71d0c237652114f66deb3b248074
AndyDinh <39890974+AndyDinh@users.noreply.github.com>
2019-05-24 00:19:59 -0400

    Create envsetup.sh

---
+++ envsetup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+# Setting up aardvark and repokid environment
+echo "Setting up environment folder for aardvark and repokid..."
+wget https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz
+
+# initializing folder
+echo "Creating separate directory..."
+mkdir ../aardvark_repokid_demo
+cd ../aardvark_repokid_demo
+mkdir aardvark_repokid
+mkdir dynamodb_local
+
+# Providing local environment for dynamodb
+echo "Unzipping local DynamoDB for environment testing..."
+cd dynamodb_local
+tar xzvf ../../setup/dynamodb_local_latest.tar.gz 
+
+# Creating aardvark/repokid dir.
+echo "Setting up Aardvark & Repokid"
+cd ../aardvark_repokid
+
+mkdir aardvark
+echo "Cloning Aardvark github environment..."
+cd aardvark
+git clone git@github.com:Netflix-Skunkworks/aardvark.git
+cd ..
+
+# copying files
+mkdir customize
+cp "../../setup/custom_configs.json" "./customize/custom_repokid_configs.json"
+cp "../../setup/base_configs.json" "./customize/default_repokid_configs.json"
+
+mkdir repokid
+echo "Cloning Repokid github environment..."
+cd repokid
+git clone git@github.com:Netflix/repokid.git
+cd ../../
+
+# Create a virtual environment Python-2.7 with environment requirements
+echo "Ensure Python2.7 is installed... Attempting to create virtual environment with Python2.7..."
+sudo apt install python2.7
+sudo apt install python2-pip
+
+python2 -m pip install virtualenv
+python2 -m virtualenv aardvark_repokid_env
